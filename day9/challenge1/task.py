3c1c8719b3904766409f1eb2116c6364f87e6042
thomas <thomas.appler@lean-coders.at>
2023-12-12 09:40:32 +0100

    day9 challenge1+2

---
+++ day9/challenge1/task.py
@@ -0,0 +1,12 @@
+
+def get_next_in_row(history):
+    if len(set(history)) == 1:
+        return history[0]
+    deltas = [history[i+1] - history[i] for i in range(len(history)-1)]
+    return history[-1] + get_next_in_row(deltas)
+
+
+file = open('input.txt', 'r')
+content = file.readlines()
+histories = [[int(x) for x in h.strip().split(" ") if len(x.strip()) > 0] for h in content if len(h.strip()) > 0]
+print(sum([get_next_in_row(history) for history in histories]))
